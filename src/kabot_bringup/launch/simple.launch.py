4ff8abf540f7b3a8902a5eb0cca9f625c67ae8b3
Krzysztof Pochwała <k.pochwala@gmail.com>
2024-12-08 00:51:39 +0100

    feat: make gazebo more verbose

--- src/kabot_bringup/launch/simple.launch.py
+++ src/kabot_bringup/launch/simple.launch.py
@@ -35,11 +35,14 @@ def generate_launch_description():
     gz_sim = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
-        launch_arguments={'gz_args': PathJoinSubstitution([
-            pkg_project_gazebo,
-            'worlds',
-            'simple.sdf'
-        ])}.items(),
+        launch_arguments={'gz_args': [
+            PathJoinSubstitution([
+                pkg_project_gazebo,
+                'worlds',
+                'simple.sdf'
+            ]),
+            ' -v4'     # Sets the verbosity level to 4
+            ]}.items(),
     )
 
     return LaunchDescription([
